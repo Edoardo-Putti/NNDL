ba31b98c7b9cf2783e0a681bcf8e98bfca8f5e32
Simone De Vita <simonedevita1997@gmail.com>
2020-11-15 16:45:58 +0100

    Modified split_dataset.py to handle validation

--- split_dataset.py
+++ split_dataset.py
@@ -1,11 +1,12 @@
 import json
 import os
 import shutil
+import random
 
-def split_dataset(training_path, classes_path, root_training_path):
+def split_dataset(training_path, classes_path, root_training_path, 
+                  root_validation_path, training_percentage=0.70):
     # training_path -> "./MaskDataset/training"
     # classes_path -> "./MaskDataset/train_gt.json"
-    # root_training_path -> new folder to be used with 'flow_from_directory'
 
     with open(classes_path) as classes_json:
         classes = json.load(classes_json)
@@ -13,18 +14,40 @@ def split_dataset(training_path, classes_path, root_training_path):
     class0 = [os.path.join(training_path, image_path) for (image_path, image_class) in classes.items() if image_class == 0]
     class1 = [os.path.join(training_path, image_path) for (image_path, image_class) in classes.items() if image_class == 1]
     class2 = [os.path.join(training_path, image_path) for (image_path, image_class) in classes.items() if image_class == 2]
+    random.shuffle(class0)
+    random.shuffle(class1)
+    random.shuffle(class2)
 
     os.makedirs(root_training_path)
-    class0_path = os.path.join(root_training_path, "class_0")
-    class1_path = os.path.join(root_training_path, "class_1")
-    class2_path = os.path.join(root_training_path, "class_2")
-    os.makedirs(class0_path)
-    os.makedirs(class1_path)
-    os.makedirs(class2_path)
+    class0_training_path = os.path.join(root_training_path, "class_0")
+    class1_training_path = os.path.join(root_training_path, "class_1")
+    class2_training_path = os.path.join(root_training_path, "class_2")
+    os.makedirs(class0_training_path)
+    os.makedirs(class1_training_path)
+    os.makedirs(class2_training_path)
 
-    for class0_image in class0:
-        shutil.copy2(class0_image, class0_path)
-    for class1_image in class1:
-        shutil.copy2(class1_image, class1_path)
-    for class2_image in class2:
-        shutil.copy2(class2_image, class2_path)
+    os.makedirs(root_validation_path)
+    class0_validation_path = os.path.join(root_validation_path, "class_0")
+    class1_validation_path = os.path.join(root_validation_path, "class_1")
+    class2_validation_path = os.path.join(root_validation_path, "class_2")
+    os.makedirs(class0_validation_path)
+    os.makedirs(class1_validation_path)
+    os.makedirs(class2_validation_path)
+    
+    class0_split = int(len(class0)*training_percentage)
+    for class0_image in class0[0:class0_split]:
+        shutil.copy2(class0_image, class0_training_path)
+    for class0_image in class0[class0_split:]:
+        shutil.copy2(class0_image, class0_validation_path)
+
+    class1_split = int(len(class1)*training_percentage)
+    for class1_image in class1[0:class1_split]:
+        shutil.copy2(class1_image, class1_training_path)
+    for class1_image in class1[class1_split:]:
+        shutil.copy2(class1_image, class1_validation_path)
+
+    class2_split = int(len(class2)*training_percentage)
+    for class2_image in class2[0:class2_split]:
+        shutil.copy2(class2_image, class2_training_path)
+    for class2_image in class2[class2_split:]:
+        shutil.copy2(class2_image, class2_training_path)
